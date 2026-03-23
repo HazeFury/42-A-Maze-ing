d75292cde92d68116064f87d1f0ce0a2eb38021b
Marc-Antoine Berger <mab.dev69@gmail.com>
2026-03-23 01:08:43 +0100

    WIP: parsing. Added some tests on MazeConfig for parsing

--- app/parser.py
+++ app/parser.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel, Field, field_validator, ValidationError
 from typing import Optional, Dict
-import sys
 
 
 class MazeConfig(BaseModel):
-    width: int = Field(alias="WIDTH", ge=4)
+    width: int = Field(alias="WIDTH", ge=4)  # Je pense qu'il faut au minimun un maze de 4x4 si on veut respecter la consigne
     height: int = Field(alias="HEIGHT", ge=4)
     entry_coord: tuple[int, int] = Field(alias="ENTRY")
     exit_coord: tuple[int, int] = Field(alias="EXIT")
@@ -21,20 +20,14 @@ class MazeConfig(BaseModel):
     # ## vérifier ENTRY != EXIT###
 
 
-def parsing_config_file() -> Dict[str, str | int | tuple[int, int]]:
+def parsing_config_file(filepath: str) -> Dict[str, str | int | tuple[int, int]]:
 
     raw_data: dict[str, str | int] = dict()
 
-    if len(sys.argv) > 2:
-        print("Too many arguments ! This project takes only one argument to work")
-        print("Try the following command : 'python3 a_maze_ing.py config.txt'")
-        sys.exit(1)
-
     try:
-        with open(sys.argv[1], "r") as config:
+        with open(filepath, "r") as config:
             for line in config:
                 if not line.startswith("#"):
-                    # print(f"-- {line}", end="")
                     parts: list[str] = line.split("=")
                     if len(parts) == 2:
                         key: str = parts[0]
@@ -42,8 +35,7 @@ def parsing_config_file() -> Dict[str, str | int | tuple[int, int]]:
                         value = value.replace("\n", "")
                         raw_data.update({key: value})
         print(raw_data)
-        data = MazeConfig(**raw_data)
-        return data
+        # data = MazeConfig(**raw_data)  // a decommenter quand le MazeConfig seras opérationnel
+        # return data
     except FileNotFoundError:
-        print(f"[ERROR] File not found : '{sys.argv[1]}'")
-        sys.exit(1)
+        raise FileNotFoundError(f"File not found : '{filepath}'")
